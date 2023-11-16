435b8d092af91d0d6441b8ff3d10aeebbe4dfe8e
Plumbiu <3434909403@qq.com>
2023-11-16 12:57:05 +0800

    feat: use vscode api

--- src/index.ts
+++ src/index.ts
@@ -7,49 +7,37 @@ import {
   commands,
   languages,
 } from 'vscode'
-import { glob } from 'fast-glob'
 import { createHint, createNeededArr, findDepsLineStart } from './utils.js'
 
 export function activate(ctx: ExtensionContext) {
-  const folders = workspace.workspaceFolders
-  if (!folders || !folders.length) {
-    return
-  }
-  const workspaces = folders
-    .map((item) => item.uri.fsPath)
   const pkgJsonMap: Map<string, InlayHint[]> = new Map()
-  for (const workspace of workspaces) {
-    glob('**/package.json', {
-      absolute: true,
-      cwd: workspace,
-      ignore: ['**/node_modules'],
-    }).then((pkgs) => {
-      for (const pkgPath of pkgs) {
-        fs.readFile(pkgPath, 'utf-8', (_err, file) => {
-          const files = file.split(/\r?\n/)
-          const { depStart, devDepStart, depEnd, devDepEnd } =
-            findDepsLineStart(files)
-          if (!depStart && !devDepStart) {
-            return
-          }
-          const hints: InlayHint[] = []
-          const needed = [
-            ...createNeededArr(files, depStart, depEnd),
-            ...createNeededArr(files, devDepStart, devDepEnd),
-          ]
-          for (const { name, idx, len } of needed) {
-            exec(`npm view ${name} version`, (_err, v) => {
-              if (!v) {
-                return
-              }
-              hints.push(createHint([idx, len], v))
-            })
-          }
-          pkgJsonMap.set(pkgPath, hints)
-        })
-      }
-    })
-  }
+  workspace.findFiles('**/package.json', '**/node_modules/**').then((uri) => {
+    for (const workspace of uri) {
+      const pkgPath = workspace.fsPath
+      fs.readFile(pkgPath, 'utf-8', (_err, file) => {
+        const files = file.split(/\r?\n/)
+        const { depStart, devDepStart, depEnd, devDepEnd } =
+          findDepsLineStart(files)
+        if (!depStart && !devDepStart) {
+          return
+        }
+        const hints: InlayHint[] = []
+        const needed = [
+          ...createNeededArr(files, depStart, depEnd),
+          ...createNeededArr(files, devDepStart, devDepEnd),
+        ]
+        for (const { name, idx, len } of needed) {
+          exec(`npm view ${name} version`, (_err, v) => {
+            if (!v) {
+              return
+            }
+            hints.push(createHint([idx, len], v))
+          })
+        }
+        pkgJsonMap.set(pkgPath, hints)
+      })
+    }
+  })
 
   commands.registerCommand('vscode-keep-package-latest.init', () => {
     languages.registerInlayHintsProvider(
@@ -58,7 +46,7 @@ export function activate(ctx: ExtensionContext) {
       },
       {
         provideInlayHints(document) {
-          return pkgJsonMap.get(document.uri.path.slice(1))
+          return pkgJsonMap.get(document.uri.fsPath)
         },
       },
     )
